3dcd10f0f82bc67b1e9a10bf17647d609a4be9be
ziller <143824815+godziller@users.noreply.github.com>
2024-01-31 15:59:28 +0000

    Update q2.py

--- S02WW03/q2.py
+++ S02WW03/q2.py
@@ -1,1 +1,13 @@
+# Without using lambdas
+def starts_with_A(s):
+    return s[0] == "A"
+fruit = ["Apple", "Banana", "Pear", "Apricot", "Orange"]
+map_object = map(starts_with_A, fruit)
+print(list(map_object))
 
+
+# Using List comprehension
+
+print("Using List Comprehension - same?")
+newList = [True if word[0] == "A" else False for word in fruit] 
+print(newList)
