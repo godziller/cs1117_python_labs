f6fcbe9a1583b1435e842bed614126465006e6f9
ziller <143824815+godziller@users.noreply.github.com>
2024-01-31 15:34:51 +0000

    Update q1.py
    Simple lambda to start

--- S02WW03/q1.py
+++ S02WW03/q1.py
@@ -1,1 +1,15 @@
+import string
+
+
+programming_languages = ('php','java','python','c++','c')
+
+print (programming_languages)
+
+# Working on lambda first
+
+capFn = lambda lang: string.upper(lang)
+
+for lang in programming_languages:
+    print(capFn(lang))
+
 
