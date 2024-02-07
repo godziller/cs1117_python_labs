47df7983421acac0fe8ca1f86efd2fa4cb726cc5
ziller <darren@counihan.ie>
2024-02-07 14:51:54 +0000

    adding question 2

--- S02WW04/voting.py
+++ S02WW04/voting.py
@@ -1,6 +1,7 @@
 
 def readVotes():
     f = open("votes.txt", 'r')
+    print("\n --Question 1-- \n")
     lineList = []
     for line in f.readlines():
         line = line.strip('\n').replace(',','').split()         #getting rid of any extra lines, commas and splitting up the words 
@@ -19,17 +20,20 @@ def countAllVotes(mat):
             number = int(number)
             rowAmount = rowAmount + number
         print(rowAmount)
-   # zipVotes(mat)
+    zipVotes(mat)
         
     
         
 def zipVotes(matrix):
-    print("ZIPVOTES")
-    for line in matrix:
-        for number in line:
-            number = int(number)
-            print(number)
-        print(type(line[0]))
+    print("\n --Question 2-- \n")
+
+    pointer = 0                         #a simple pointer to move along our while loop
+    while pointer in range(0,4):        #while the pointer is in range of the amount of candidates we have 
+        amount = 0
+        for line in matrix:             # for each sublist in our matrix
+            amount = amount + int(line[pointer])        # constructing a variable to act as a 'wallet' to store the sum of each number
+        print(f"Candidate {pointer}: {amount}")         # print this as your candidate's sum of votes, then repeat until out of range, ie. no more candidates
+        pointer += 1                    #inc pointer 
 
 
 readVotes()
